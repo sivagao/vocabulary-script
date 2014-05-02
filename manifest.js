2ae9e6e6b02ffbdcd9a0db29b309d849a351be04
gaohailang <gaohailang@wandoujia.com>
2014-05-02 23:48:53 +0800

    feat: add mainifest.js for geasemonkey engine

---
+++ manifest.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name       youdao fanyi new word books
+// @namespace  http://weibo.com/1siva
+// @version    0.1
+// @description  easily add new words detected by youdao
+// @match      *://*/*
+// @copyright  2014+, SivaGao(gaohailang@wandoujia.com)
+// ==/UserScript==
+
